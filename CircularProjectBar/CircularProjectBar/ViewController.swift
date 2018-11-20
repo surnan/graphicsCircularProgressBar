a073d36ebdec33ef5acfe30f00274eeeb60afe25
surnan <forsuresh@yandex.com>
2018-11-20 11:37:46 -0500

    Starts successfully without storyboard

---
+++ CircularProjectBar/CircularProjectBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircularProjectBar
+//
+//  Created by admin on 11/20/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
