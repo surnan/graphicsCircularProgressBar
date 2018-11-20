5358d6eead3cf7684d18f6e7263385e7f977b44c
surnan <forsuresh@yandex.com>
2018-11-20 13:05:00 -0500

    Circle created via UIBezierpath Tap Gesture added shapeLayer: CAShapeLayer used to customize circle's settings

--- CircularProjectBar/CircularProjectBar/TestController.swift
+++ CircularProjectBar/CircularProjectBar/TestController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class TestController: UIViewController {
 
+    
+    let shapeLayer = CAShapeLayer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.yellow
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    
+        //drawing circle
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        
+        
+        view.layer.addSublayer(shapeLayer)
     }
 
+    
+    
+    @objc func handleTap(){
+        print("--TAP registered--")
+        
+        shapeLayer.strokeEnd = 0
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        
+        
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBAsic")
+    }
 
 }
 
